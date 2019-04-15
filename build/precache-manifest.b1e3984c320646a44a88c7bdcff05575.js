7e4254117296454a151b69fe638116ab225c9601
Jacob <jakeguss@gmail.com>
2019-04-14 20:50:57 -0600

    zero to heroku

---
+++ build/precache-manifest.b1e3984c320646a44a88c7bdcff05575.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9833194b3a8e58ec5282",
+    "url": "/static/js/main.9833194b.chunk.js"
+  },
+  {
+    "revision": "f7b477dfa4c87d98b2e6",
+    "url": "/static/js/1.f7b477df.chunk.js"
+  },
+  {
+    "revision": "9833194b3a8e58ec5282",
+    "url": "/static/css/main.0b989f4b.chunk.css"
+  },
+  {
+    "revision": "aaf16c406bcb595016f82e414b0b7be5",
+    "url": "/index.html"
+  }
+];
